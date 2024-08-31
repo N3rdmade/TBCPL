d906812e943918dee563301f1e31276c559f1372
Rishab <84559232+RISHAB-CREATOR@users.noreply.github.com>
2024-08-31 22:36:55 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,51 @@
+const CACHE_NAME = 'tbcpl-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/404.html',
+  '/robots.txt',
+  '/manifest.json',
+  '/logo.png',
+  '.htaccess',
+  '/css/flash.css',
+  '/js/smoothscroll.js',
+  '/js/nocheats.js',
+  '/js/protected_devtoolsdetector.js'
+];
+
+// Install a Service Worker
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches.open('tbcpl-cache-v1').then((cache) => {
-        return cache.addAll([
-          '/',
-          '/index.html',     
-          '/404.html',
-          '/.htaccess',
-          '/js/protected_devtoolsdetector.js',
-          '/js/nocheats.js',
-          '/js/smoothscroll.js',
-          '/css/flash.css',
-        ]);
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
       })
-    );
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
+  );
+});
+
+// Cache and return requests
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
         return response || fetch(event.request);
       })
-    );
-  });
-  
+  );
+});
+
+// Update a Service Worker
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
