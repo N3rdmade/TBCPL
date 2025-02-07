c1cd657023d8747be98abb2fdc69e58b35d198d6
Rishab <84559232+RISHAB-CREATOR@users.noreply.github.com>
2025-02-08 02:32:41 +0530

    Update and rename service-worker.js to sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('tbcpl-cache-v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/logo.png',
+        '/manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
