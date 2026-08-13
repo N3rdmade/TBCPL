31ae83503e847ed8177c13bf53b48e3c9a6d297d
rishabnotfound <itzrishabboss@gmail.com>
2026-08-13 21:32:19 +0530

    drop standalone output, disable image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
   images: {
